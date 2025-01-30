24e3201f89531097ecf176a9eb32408979152ccc
Gerald Ofori <gerald.ofori@hubtel.com>
2025-01-30 22:01:42 +0000

    done with car component and api integration

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,18 +5,30 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
-  btnType
+  btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       className={`custom-btn ${containerStyles}`}
-      type={btnType ||`button`}
+      type={btnType || `button`}
       onClick={() => {
         handleClick;
       }}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
